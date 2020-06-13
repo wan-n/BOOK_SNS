72beb9f8594693ecd8291c0016f517296e5576b3
eunji <dododidi@gc.gachon.ac.kr>
2020-06-13 18:04:26 +0900

    ok

---
+++ app/src/main/java/com/example/instabook/Activity/ForHome/UserBookUIDData.java
@@ -0,0 +1,17 @@
+package com.example.instabook.Activity.ForHome;
+
+public class UserBookUIDData {
+    public int UserBookUID;
+
+    public UserBookUIDData(int ubuid) {
+        this.UserBookUID = ubuid;
+    }
+
+    public int getUserBookUID() {
+        return UserBookUID;
+    }
+
+    public void setUserBookUID(int userBookUID) {
+        UserBookUID = userBookUID;
+    }
+}
